562a6a7cca78ec89f565f0b2533b6b18e9494f4e
Tyooo <88655862+IniTyoo@users.noreply.github.com>
2022-11-01 22:56:32 +0700

    Update asdasdasd.cc

--- asdasdasd.cc
+++ asdasdasd.cc
@@ -1,1 +1,1 @@
-[{"name":"test","status":"Active","ip":"192.23.*** ( RDP 1 ) "}, {"name":"test2","status":"Inactive","ip":"192.23.*** ( RDP 2 ) "}]
+[{"name":"test","status":"Active","ip":"192.23.*** ( RDP 1 ) "}, {"name":"test2","status":"Inactive","ip":"192.23.*** ( RDP 2 ) "}, {"name":"test3","status":"Inactive","ip":"22.23.*** ( RDP 3 ) "}]
